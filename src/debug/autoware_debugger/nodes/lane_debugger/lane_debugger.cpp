563cddcb0fd384add087a2cc522c40c8163135fd
Yusuke FUJII <yusuke.fujii@tier4.jp>
2018-05-13 20:25:52 +0900

    support waypoint_debugger for current_autoware_version

--- src/debug/autoware_debugger/nodes/lane_debugger/lane_debugger.cpp
+++ src/debug/autoware_debugger/nodes/lane_debugger/lane_debugger.cpp
@@ -9,32 +9,29 @@
 #include <string>
 #include <vector>
 
-#include <dynamic_reconfigure/server.h>
-#include <waypoint_debugger/WaypointDebuggerConfig.h>
 #include "autoware_msgs/ConfigLaneStop.h"
 #include "autoware_msgs/LaneArray.h"
 #include "autoware_msgs/traffic_light.h"
 #include "waypoint_follower/libwaypoint_follower.h"
+#include <dynamic_reconfigure/server.h>
+#include <waypoint_debugger/WaypointDebuggerConfig.h>
 
+#include <amathutils_lib/amathutils.hpp>
 #include <ros/console.h>
 #include <vector_map/vector_map.h>
-#include <amathutils_lib/amathutils.hpp>
 
-namespace autoware_debug
-{
+namespace autoware_debug {
 using namespace vector_map;
-geometry_msgs::Point to_geoPoint(const vector_map_msgs::Point& vp)
-{
+geometry_msgs::Point to_geoPoint(const vector_map_msgs::Point &vp) {
   geometry_msgs::Point gp;
   gp.x = vp.ly;
   gp.y = vp.bx;
   gp.z = vp.h;
   return gp;
 }
-class LaneDebugger
-{
+class LaneDebugger {
 private:
-  tf::TransformListener* tflistener_;
+  tf::TransformListener *tflistener_;
   std::map<std::string, ros::Subscriber> Subs_;
   std::map<std::string, ros::Publisher> Pubs_;
   vector_map::VectorMap vmap_;
@@ -49,8 +46,7 @@ private:
   std_msgs::ColorRGBA color_blue_;
 
 public:
-  LaneDebugger()
-  {
+  LaneDebugger() {
     color_green_.r = 0.2;
     color_green_.g = 1.0;
     color_green_.b = 0.2;
@@ -63,49 +59,49 @@ public:
 
     color_green_.a = color_red_.a = color_blue_.a = 1.0;
   }
-  ~LaneDebugger()
-  {
+  ~LaneDebugger() {
     if (tflistener_)
       delete tflistener_;
   }
 
-  std::vector<geometry_msgs::Point> getNearestDTLane(geometry_msgs::PointStamped pt)
-  {
+  std::vector<geometry_msgs::Point>
+  getNearestDTLane(geometry_msgs::PointStamped pt) {
     std::vector<geometry_msgs::Point> ret;
-    std::vector<DTLane> DTLanes = vmap_.findByFilter([](const DTLane& dtlane) { return true; });
+    std::vector<DTLane> DTLanes =
+        vmap_.findByFilter([](const DTLane &dtlane) { return true; });
 
     geometry_msgs::PointStamped current_pt_map;
 
     tflistener_->transformPoint("map", pt, current_pt_map);
 
-    amathutils::point current_point(current_pt_map.point.x, current_pt_map.point.y, 0.0);
     vector_map_msgs::DTLane closest_dtlane;
     vector_map_msgs::Point closest_pid;
     double closest_distance = 999999.999;
-    for (auto& dtlane : DTLanes)
-    {
-      vector_map_msgs::Point dtlane_pid = vmap_.findByKey(Key<Point>(dtlane.pid));
-      amathutils::point p1(dtlane_pid.ly, dtlane_pid.bx, 0.0);
-      double distance = amathutils::find_distance(&current_point, &p1);
-      if (distance < closest_distance)
-      {
+    for (auto &dtlane : DTLanes) {
+      vector_map_msgs::Point dtlane_pid =
+          vmap_.findByKey(Key<Point>(dtlane.pid));
+      geometry_msgs::Point p1;
+      p1.x = dtlane_pid.ly;
+      p1.y = dtlane_pid.bx;
+      double distance = amathutils::find_distance(current_pt_map.point, p1);
+      if (distance < closest_distance) {
         closest_dtlane = dtlane;
         closest_pid = dtlane_pid;
         closest_distance = distance;
       }
     }
 
-    std::vector<Lane> Lanes = vmap_.findByFilter([](const Lane& lane) { return true; });
-    std::vector<Lane> Lanes_lnids =
-        vmap_.findByFilter([&](const Lane& lane) { return lane.did == closest_dtlane.did; });
+    std::vector<Lane> Lanes =
+        vmap_.findByFilter([](const Lane &lane) { return true; });
+    std::vector<Lane> Lanes_lnids = vmap_.findByFilter(
+        [&](const Lane &lane) { return lane.did == closest_dtlane.did; });
 
     if (Lanes_lnids.empty())
       return std::vector<geometry_msgs::Point>();
 
     Lane target_lane = Lanes_lnids.at(0);
     Lane prev_lane = vmap_.findByKey(Key<Lane>(target_lane.lnid));
-    do
-    {
+    do {
       DTLane _dtlane = vmap_.findByKey(Key<DTLane>(prev_lane.did));
       Point lane_pid = vmap_.findByKey(Key<Point>(_dtlane.pid));
       geometry_msgs::Point pt = to_geoPoint(lane_pid);
@@ -114,8 +110,7 @@ public:
     } while (prev_lane.lnid != 0 && prev_lane.jct == 0);
 
     Lane next_lane = vmap_.findByKey(Key<Lane>(target_lane.lnid));
-    do
-    {
+    do {
       DTLane _dtlane = vmap_.findByKey(Key<DTLane>(next_lane.did));
       Point lane_pid = vmap_.findByKey(Key<Point>(_dtlane.pid));
       geometry_msgs::Point pt = to_geoPoint(lane_pid);
@@ -125,8 +120,7 @@ public:
 
     return ret;
   }
-  void clickedPointCallback(const geometry_msgs::PointStamped& msg)
-  {
+  void clickedPointCallback(const geometry_msgs::PointStamped &msg) {
     std::vector<geometry_msgs::Point> closestDtl = getNearestDTLane(msg);
 
     visualization_msgs::Marker dtlane_line_marker;
@@ -145,44 +139,45 @@ public:
     dtlane_line_marker.color.b = 0.2;
     dtlane_line_marker.lifetime = ros::Duration(0);
 
-    for (auto& lane : closestDtl)
-    {
+    for (auto &lane : closestDtl) {
       dtlane_line_marker.points.push_back(lane);
     }
     lanes_marker_array.markers.push_back(dtlane_line_marker);
     Pubs_["lane_debug"].publish(lanes_marker_array);
     lanes_marker_array.markers.clear();
   }
-  void currentPoseCallback(const geometry_msgs::PoseStamped& msg)
-  {
+  void currentPoseCallback(const geometry_msgs::PoseStamped &msg) {
     geometry_msgs::PointStamped pt;
     pt.header = msg.header;
     pt.point = msg.pose.position;
     clickedPointCallback(pt);
   }
 
-  void init()
-  {
+  void init() {
     vmap_.subscribe(nh_,
-                    Category::POINT | Category::LINE | Category::VECTOR | Category::AREA |
-                        Category::POLE |  // basic class
-                        Category::DTLANE | Category::LANE | Category::NODE | Category::STOP_LINE | Category::ROAD_SIGN);
+                    Category::POINT | Category::LINE | Category::VECTOR |
+                        Category::AREA | Category::POLE | // basic class
+                        Category::DTLANE | Category::LANE | Category::NODE |
+                        Category::STOP_LINE | Category::ROAD_SIGN);
 
     tflistener_ = new tf::TransformListener();
 
-    //    Subs_["lane_waypoints_array"] = nh_.subscribe("lane_waypoints_array", 10, laneArrayCallback);
-    //    Subs_["closest_waypoint"] = nh_.subscribe("closest_waypoint", 1, closestWaypointCallback);
-    Subs_["current_pose"] = nh_.subscribe("current_pose", 1, &LaneDebugger::currentPoseCallback, this);
-    Subs_["clicked_point"] = nh_.subscribe("/clicked_point", 10, &LaneDebugger::clickedPointCallback, this);
+    //    Subs_["lane_waypoints_array"] = nh_.subscribe("lane_waypoints_array",
+    //    10, laneArrayCallback);
+    //    Subs_["closest_waypoint"] = nh_.subscribe("closest_waypoint", 1,
+    //    closestWaypointCallback);
+    Subs_["current_pose"] = nh_.subscribe(
+        "current_pose", 1, &LaneDebugger::currentPoseCallback, this);
+    Subs_["clicked_point"] = nh_.subscribe(
+        "/clicked_point", 10, &LaneDebugger::clickedPointCallback, this);
 
-    Pubs_["lane_debug"] = nh_.advertise<visualization_msgs::MarkerArray>("lane_debug", 9, true);
+    Pubs_["lane_debug"] =
+        nh_.advertise<visualization_msgs::MarkerArray>("lane_debug", 9, true);
   }
-  void run()
-  {
+  void run() {
     init();
     ros::Rate r(2);
-    while (ros::ok())
-    {
+    while (ros::ok()) {
       ros::spinOnce();
       r.sleep();
     }
@@ -192,8 +187,7 @@ public:
 
 using namespace autoware_debug;
 
-int main(int argc, char** argv)
-{
+int main(int argc, char **argv) {
   ros::init(argc, argv, "LaneDebuggerNode");
   autoware_debug::LaneDebugger lane_debugger;
   lane_debugger.run();
--- src/debug/autoware_debugger/nodes/waypoint_debugger/waypoint_debugger.cpp
+++ src/debug/autoware_debugger/nodes/waypoint_debugger/waypoint_debugger.cpp
@@ -41,25 +41,26 @@ visualization_msgs::MarkerArray g_local_waypoints_marker_array;
 
 autoware_msgs::LaneArray aLaneArray;
 
-tf::TransformListener *tflistener;
+tf::TransformListener* tflistener;
 
 int g_config_count = 0;
 
-namespace autoware_debug {
-void publishMarker() {
+namespace autoware_debug
+{
+void publishMarker()
+{
   visualization_msgs::MarkerArray marker_array;
 
   // insert global marker
-  marker_array.markers.insert(marker_array.markers.end(),
-                              g_local_waypoints_marker_array.markers.begin(),
+  marker_array.markers.insert(marker_array.markers.end(), g_local_waypoints_marker_array.markers.begin(),
                               g_local_waypoints_marker_array.markers.end());
 
   g_global_mark_pub.publish(marker_array);
   g_local_waypoints_marker_array.markers.clear();
 }
 
-void createWaypointMarker(
-    const autoware_msgs::LaneArray &lane_waypoints_array) {
+void createWaypointMarker(const autoware_msgs::LaneArray& lane_waypoints_array)
+{
   visualization_msgs::Marker lane_waypoint_marker;
   lane_waypoint_marker.header.frame_id = "map";
   lane_waypoint_marker.header.stamp = ros::Time();
@@ -76,9 +77,12 @@ void createWaypointMarker(
 
   int count = 0;
 
-  for (auto lane : lane_waypoints_array.lanes) {
-    for (unsigned int i = 0; i < lane.waypoints.size(); i++) {
-      if (count++ == g_config_count) {
+  for (auto lane : lane_waypoints_array.lanes)
+  {
+    for (unsigned int i = 0; i < lane.waypoints.size(); i++)
+    {
+      if (count++ == g_config_count)
+      {
         geometry_msgs::Point point;
         point = lane.waypoints[i].pose.pose.position;
         lane_waypoint_marker.points.push_back(point);
@@ -108,13 +112,16 @@ void createWaypointMarker(
   stop_sign.type = visualization_msgs::Marker::MESH_RESOURCE;
   stop_sign.mesh_resource = "package://waypoint_debugger/imgs/stop.dae";
   stop_sign.mesh_use_embedded_materials = true;
-  for (auto lane : lane_waypoints_array.lanes) {
-    for (unsigned int i = 0; i < lane.waypoints.size(); i++) {
+  for (auto lane : lane_waypoints_array.lanes)
+  {
+    for (unsigned int i = 0; i < lane.waypoints.size(); i++)
+    {
       geometry_msgs::Point point;
       point = lane.waypoints[i].pose.pose.position;
       point.z += 0.5;
 
-      if (lane.waypoints[i].wpstate.stop_state != 0) {
+      if (lane.waypoints[i].wpstate.stop_state != 0)
+      {
         lane_waypoint_marker.points.push_back(point);
         stop_sign.pose = lane.waypoints[i].pose.pose;
         stop_sign.pose.position.z += 2;
@@ -127,20 +134,22 @@ void createWaypointMarker(
   }
 }
 
-std::string getSteerText(unsigned int &state) {
-  switch (state) {
-  case (autoware_msgs::WaypointState::STR_LEFT):
-    return "left";
-  case (autoware_msgs::WaypointState::STR_RIGHT):
-    return "right";
-  case (autoware_msgs::WaypointState::STR_STRAIGHT):
-    return "straight";
+std::string getSteerText(unsigned int& state)
+{
+  switch (state)
+  {
+    case (autoware_msgs::WaypointState::STR_LEFT):
+      return "left";
+    case (autoware_msgs::WaypointState::STR_RIGHT):
+      return "right";
+    case (autoware_msgs::WaypointState::STR_STRAIGHT):
+      return "straight";
   };
   return "unknown";
 }
 
-void createWaypointLabelMarker(
-    const autoware_msgs::LaneArray &lane_waypoints_array) {
+void createWaypointLabelMarker(const autoware_msgs::LaneArray& lane_waypoints_array)
+{
   visualization_msgs::Marker waypoint_label_marker;
   waypoint_label_marker.header.frame_id = "map";
   waypoint_label_marker.header.stamp = ros::Time();
@@ -158,25 +167,20 @@ void createWaypointLabelMarker(
   waypoint_label_marker.lifetime = ros::Duration(10);
   int id;
 
-  for (auto &lane : lane_waypoints_array.lanes) {
-    for (size_t wpi = 1; wpi < lane.waypoints.size(); wpi++) {
+  for (auto& lane : lane_waypoints_array.lanes)
+  {
+    for (size_t wpi = 1; wpi < lane.waypoints.size(); wpi++)
+    {
       waypoint_label_marker.id = ++id;
-      waypoint_label_marker.pose.position =
-          lane.waypoints.at(wpi).pose.pose.position;
+      waypoint_label_marker.pose.position = lane.waypoints.at(wpi).pose.pose.position;
       waypoint_label_marker.pose.position.z += 2.0;
 
       double v0 = lane.waypoints.at(wpi - 1).twist.twist.linear.x;
       double v = lane.waypoints.at(wpi).twist.twist.linear.x;
-      amathutils::point p0(lane.waypoints.at(wpi).pose.pose.position.x,
-                           lane.waypoints.at(wpi).pose.pose.position.y,
-                           lane.waypoints.at(wpi).pose.pose.position.z);
-      amathutils::point p1(lane.waypoints.at(wpi - 1).pose.pose.position.x,
-                           lane.waypoints.at(wpi - 1).pose.pose.position.y,
-                           lane.waypoints.at(wpi - 1).pose.pose.position.z);
 
-      double distance = amathutils::find_distance(&p0, &p1);
-      double ag = amathutils::getGravityAcceleration(
-          amathutils::getAcceleration(v0, v, distance));
+      double distance =
+          amathutils::find_distance(lane.waypoints.at(wpi).pose.pose, lane.waypoints.at(wpi - 1).pose.pose);
+      double ag = amathutils::getGravityAcceleration(amathutils::getAcceleration(v0, v, distance));
 
       waypoint_label_marker.color.g = 0.5 + ag * 2;
       waypoint_label_marker.color.b = 0.5 + ag;
@@ -184,15 +188,14 @@ void createWaypointLabelMarker(
       std::ostringstream strs;
       strs << ag;
       waypoint_label_marker.text =
-          getSteerText(
-              (unsigned int &)lane.waypoints.at(wpi).wpstate.steering_state) +
-          "\n" + strs.str();
+          getSteerText((unsigned int&)lane.waypoints.at(wpi).wpstate.steering_state) + "\n" + strs.str();
       g_local_waypoints_marker_array.markers.push_back(waypoint_label_marker);
     }
   }
 }
 
-void createSignMarker() {
+void createSignMarker()
+{
   visualization_msgs::Marker stopline_marker;
   stopline_marker.header.frame_id = "map";
   stopline_marker.header.stamp = ros::Time();
@@ -208,15 +211,13 @@ void createSignMarker() {
   stopline_marker.color.b = 0.5;
   stopline_marker.lifetime = ros::Duration(10);
 
-  stopline_marker.pose.position.x =
-      stopline_marker.points.at(stopline_marker.points.size() - 1).x;
-  stopline_marker.pose.position.y =
-      stopline_marker.points.at(stopline_marker.points.size() - 1).y;
-  stopline_marker.pose.position.z =
-      stopline_marker.points.at(stopline_marker.points.size() - 1).z + 1;
+  stopline_marker.pose.position.x = stopline_marker.points.at(stopline_marker.points.size() - 1).x;
+  stopline_marker.pose.position.y = stopline_marker.points.at(stopline_marker.points.size() - 1).y;
+  stopline_marker.pose.position.z = stopline_marker.points.at(stopline_marker.points.size() - 1).z + 1;
 }
 
-geometry_msgs::Point to_geoPoint(const vector_map_msgs::Point &vp) {
+geometry_msgs::Point to_geoPoint(const vector_map_msgs::Point& vp)
+{
   geometry_msgs::Point gp;
   gp.x = vp.ly;
   gp.y = vp.bx;
@@ -224,7 +225,8 @@ geometry_msgs::Point to_geoPoint(const vector_map_msgs::Point &vp) {
   return gp;
 }
 
-void createStoplineMarkerByLane() {
+void createStoplineMarkerByLane()
+{
   int i = 0;
 
   visualization_msgs::Marker stopline_marker;
@@ -248,22 +250,22 @@ void createStoplineMarkerByLane() {
   visualization_msgs::Marker based_marker = stopline_marker;
   geometry_msgs::Point null_pt;
 
-  std::vector<StopLine> stoplines =
-      g_vmap.findByFilter([](const StopLine &stopline) { return true; });
-  for (auto &stopline : stoplines) {
+  std::vector<StopLine> stoplines = g_vmap.findByFilter([](const StopLine& stopline) { return true; });
+  for (auto& stopline : stoplines)
+  {
     stopline_marker = based_marker;
-    if (g_vmap.findByKey(Key<RoadSign>(stopline.signid)).type ==
-        vector_map_msgs::RoadSign::TYPE_STOP) {
+    if (g_vmap.findByKey(Key<RoadSign>(stopline.signid)).type == vector_map_msgs::RoadSign::TYPE_STOP)
+    {
       stopline_marker.color.r = 1;
       stopline_marker.color.g = 0;
       stopline_marker.color.b = 1;
-    } else {
+    }
+    else
+    {
       stopline_marker.color = based_marker.color;
     }
-    geometry_msgs::Point bp = to_geoPoint(g_vmap.findByKey(
-        Key<Point>(g_vmap.findByKey(Key<Line>(stopline.lid)).bpid)));
-    geometry_msgs::Point fp = to_geoPoint(g_vmap.findByKey(
-        Key<Point>(g_vmap.findByKey(Key<Line>(stopline.lid)).fpid)));
+    geometry_msgs::Point bp = to_geoPoint(g_vmap.findByKey(Key<Point>(g_vmap.findByKey(Key<Line>(stopline.lid)).bpid)));
+    geometry_msgs::Point fp = to_geoPoint(g_vmap.findByKey(Key<Point>(g_vmap.findByKey(Key<Line>(stopline.lid)).fpid)));
 
     stopline_marker.points.push_back(bp);
     stopline_marker.points.push_back(fp);
@@ -279,8 +281,7 @@ void createStoplineMarkerByLane() {
 
     stopline_marker.ns = "label";
     stopline_marker.pose.position = cp;
-    stopline_marker.pose.position.z =
-        stopline_marker.points.at(stopline_marker.points.size() - 1).z + 2;
+    stopline_marker.pose.position.z = stopline_marker.points.at(stopline_marker.points.size() - 1).z + 2;
     /*
     stopline_marker.pose.position.x =
     stopline_marker.points.at(stopline_marker.points.size()-1).x;
@@ -290,10 +291,8 @@ void createStoplineMarkerByLane() {
     stopline_marker.points.at(stopline_marker.points.size()-1).z + 1;
     */
     stopline_marker.type = visualization_msgs::Marker::TEXT_VIEW_FACING;
-    stopline_marker.text =
-        std::to_string(stopline.id) + "::" + std::to_string(stopline.signid) +
-        "::" +
-        std::to_string(g_vmap.findByKey(Key<RoadSign>(stopline.signid)).type);
+    stopline_marker.text = std::to_string(stopline.id) + "::" + std::to_string(stopline.signid) + "::" +
+                           std::to_string(g_vmap.findByKey(Key<RoadSign>(stopline.signid)).type);
     stopline_marker.id = i++;
 
     stopline_marker.points.clear();
@@ -310,7 +309,8 @@ void createStoplineMarkerByLane() {
   }
 }
 
-void configParameter(const std_msgs::Int32 &msg) {
+void configParameter(const std_msgs::Int32& msg)
+{
   g_config_count = msg.data;
   g_local_waypoints_marker_array.markers.clear();
 
@@ -319,13 +319,14 @@ void configParameter(const std_msgs::Int32 &msg) {
   publishMarker();
 }
 
-void callbackDynamicParam(WaypointDebugger::WaypointDebuggerConfig &config,
-                          uint32_t level) {
+void callbackDynamicParam(WaypointDebugger::WaypointDebuggerConfig& config, uint32_t level)
+{
   g_config_count = config.int_param;
   ROS_INFO("set dynamicparam");
 }
 
-void laneArrayCallback(const autoware_msgs::LaneArrayConstPtr &msg) {
+void laneArrayCallback(const autoware_msgs::LaneArrayConstPtr& msg)
+{
   g_local_waypoints_marker_array.markers.clear();
 
   aLaneArray = *msg;
@@ -352,44 +353,43 @@ void laneArrayCallback(const autoware_msgs::LaneArrayConstPtr &msg) {
 int closestWaypointID_ = -1;
 visualization_msgs::MarkerArray areas_marker_array;
 
-std::vector<geometry_msgs::Point>
-getNearestDTLane(geometry_msgs::PointStamped pt) {
+std::vector<geometry_msgs::Point> getNearestDTLane(geometry_msgs::PointStamped pt)
+{
   std::vector<geometry_msgs::Point> ret;
-  std::vector<DTLane> DTLanes =
-      g_vmap.findByFilter([](const DTLane &dtlane) { return true; });
+  std::vector<DTLane> DTLanes = g_vmap.findByFilter([](const DTLane& dtlane) { return true; });
 
   geometry_msgs::PointStamped current_pt_map;
 
   tflistener->transformPoint("map", pt, current_pt_map);
 
-  amathutils::point current_point(current_pt_map.point.x,
-                                  current_pt_map.point.y, 0.0);
   vector_map_msgs::DTLane closest_dtlane;
   vector_map_msgs::Point closest_pid;
   double closest_distance = 999999.999;
-  for (auto &dtlane : DTLanes) {
-    vector_map_msgs::Point dtlane_pid =
-        g_vmap.findByKey(Key<Point>(dtlane.pid));
-    amathutils::point p1(dtlane_pid.ly, dtlane_pid.bx, 0.0);
-    double distance = amathutils::find_distance(&current_point, &p1);
-    if (distance < closest_distance) {
+  for (auto& dtlane : DTLanes)
+  {
+    vector_map_msgs::Point dtlane_pid = g_vmap.findByKey(Key<Point>(dtlane.pid));
+    geometry_msgs::Point p1;
+    p1.x = dtlane_pid.ly;
+    p1.y = dtlane_pid.bx;
+    double distance = amathutils::find_distance(current_pt_map.point, p1);
+    if (distance < closest_distance)
+    {
       closest_dtlane = dtlane;
       closest_pid = dtlane_pid;
       closest_distance = distance;
     }
   }
 
-  std::vector<Lane> Lanes =
-      g_vmap.findByFilter([](const Lane &lane) { return true; });
-  std::vector<Lane> Lanes_lnids = g_vmap.findByFilter(
-      [&](const Lane &lane) { return lane.did == closest_dtlane.did; });
+  std::vector<Lane> Lanes = g_vmap.findByFilter([](const Lane& lane) { return true; });
+  std::vector<Lane> Lanes_lnids = g_vmap.findByFilter([&](const Lane& lane) { return lane.did == closest_dtlane.did; });
 
   if (Lanes_lnids.empty())
     return std::vector<geometry_msgs::Point>();
 
   Lane target_lane = Lanes_lnids.at(0);
   Lane prev_lane = g_vmap.findByKey(Key<Lane>(target_lane.lnid));
-  do {
+  do
+  {
     DTLane _dtlane = g_vmap.findByKey(Key<DTLane>(prev_lane.did));
     Point lane_pid = g_vmap.findByKey(Key<Point>(_dtlane.pid));
     geometry_msgs::Point pt = to_geoPoint(lane_pid);
@@ -398,7 +398,8 @@ getNearestDTLane(geometry_msgs::PointStamped pt) {
   } while (prev_lane.lnid != 0 && prev_lane.jct == 0);
 
   Lane next_lane = g_vmap.findByKey(Key<Lane>(target_lane.lnid));
-  do {
+  do
+  {
     DTLane _dtlane = g_vmap.findByKey(Key<DTLane>(next_lane.did));
     Point lane_pid = g_vmap.findByKey(Key<Point>(_dtlane.pid));
     geometry_msgs::Point pt = to_geoPoint(lane_pid);
@@ -408,7 +409,8 @@ getNearestDTLane(geometry_msgs::PointStamped pt) {
 
   return ret;
 }
-void clickedPointCallback(const geometry_msgs::PointStamped &msg) {
+void clickedPointCallback(const geometry_msgs::PointStamped& msg)
+{
   ROS_INFO("[%s]", __func__);
 
   std::vector<geometry_msgs::Point> closestDtl = getNearestDTLane(msg);
@@ -429,56 +431,51 @@ void clickedPointCallback(const geometry_msgs::PointStamped &msg) {
   dtlane_line_marker.color.b = 0.2;
   dtlane_line_marker.lifetime = ros::Duration(0);
 
-  for (auto &lane : closestDtl) {
+  for (auto& lane : closestDtl)
+  {
     dtlane_line_marker.points.push_back(lane);
   }
   areas_marker_array.markers.push_back(dtlane_line_marker);
   area_pub.publish(areas_marker_array);
   areas_marker_array.markers.clear();
 }
-void currentPoseCallback(const geometry_msgs::PoseStamped &msg) {
+void currentPoseCallback(const geometry_msgs::PoseStamped& msg)
+{
   geometry_msgs::PointStamped pt;
   pt.header = msg.header;
   pt.point = msg.pose.position;
   clickedPointCallback(pt);
 }
-void closestWaypointCallback(const std_msgs::Int32 &msg) {
+void closestWaypointCallback(const std_msgs::Int32& msg)
+{
   closestWaypointID_ = msg.data;
 }
 }
 using namespace autoware_debug;
 
-int main(int argc, char **argv) {
+int main(int argc, char** argv)
+{
   ros::init(argc, argv, "waypoints_debugger");
   ros::NodeHandle nh;
 
   g_vmap.subscribe(nh,
-                   Category::POINT | Category::LINE | Category::VECTOR |
-                       Category::AREA | Category::POLE | // basic class
-                       Category::DTLANE | Category::LANE | Category::NODE |
-                       Category::STOP_LINE | Category::ROAD_SIGN);
+                   Category::POINT | Category::LINE | Category::VECTOR | Category::AREA |
+                       Category::POLE |  // basic class
+                       Category::DTLANE | Category::LANE | Category::NODE | Category::STOP_LINE | Category::ROAD_SIGN);
 
   tflistener = new tf::TransformListener();
   // subscribe global waypoints
-  ros::Subscriber lane_array_sub =
-      nh.subscribe("lane_waypoints_array", 10, laneArrayCallback);
-  ros::Subscriber closest_sub =
-      nh.subscribe("closest_waypoint", 1, closestWaypointCallback);
-  ros::Subscriber current_pose_sub =
-      nh.subscribe("current_pose", 1, currentPoseCallback);
-  ros::Subscriber config_sub =
-      nh.subscribe("config/debug/waycount", 10, configParameter);
-  ros::Subscriber clicked_sub =
-      nh.subscribe("/clicked_point", 10, clickedPointCallback);
-  g_global_mark_pub = nh.advertise<visualization_msgs::MarkerArray>(
-      "waypoints_debug", 10, true);
+  ros::Subscriber lane_array_sub = nh.subscribe("lane_waypoints_array", 10, laneArrayCallback);
+  ros::Subscriber closest_sub = nh.subscribe("closest_waypoint", 1, closestWaypointCallback);
+  ros::Subscriber current_pose_sub = nh.subscribe("current_pose", 1, currentPoseCallback);
+  ros::Subscriber config_sub = nh.subscribe("config/debug/waycount", 10, configParameter);
+  ros::Subscriber clicked_sub = nh.subscribe("/clicked_point", 10, clickedPointCallback);
+  g_global_mark_pub = nh.advertise<visualization_msgs::MarkerArray>("waypoints_debug", 10, true);
 
-  area_pub =
-      nh.advertise<visualization_msgs::MarkerArray>("areas_debug", 10, true);
+  area_pub = nh.advertise<visualization_msgs::MarkerArray>("areas_debug", 10, true);
 
   dynamic_reconfigure::Server<WaypointDebugger::WaypointDebuggerConfig> server;
-  dynamic_reconfigure::Server<
-      WaypointDebugger::WaypointDebuggerConfig>::CallbackType f =
+  dynamic_reconfigure::Server<WaypointDebugger::WaypointDebuggerConfig>::CallbackType f =
       boost::bind(&callbackDynamicParam, _1, _2);
   server.setCallback(f);
 
@@ -492,7 +489,8 @@ int main(int argc, char **argv) {
   g_local_color.a = g_local_alpha;
 
   ros::Rate r(2);
-  while (ros::ok()) {
+  while (ros::ok())
+  {
     ros::spinOnce();
     createStoplineMarkerByLane();
     createWaypointMarker(aLaneArray);
